ad4bf444b49679a8c5c92dc16d21aad0a4c16e64
Walter Cheng <walterpakwai@live.com>
2018-01-27 14:50:49 -0500

    getting html and flask

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 #importing library for the server
-from flask import Flask 
-app = Flask(__name__)
+from flask import Flask, flash, redirect, render_template, request, session, abort
+app = Flask(__name__, template_folder='html')
 
-@app.route("/")
-def hello():
-	return "Hello world!"
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.figure import Figure
+
+@app.route('/')
+def home():
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.run()
