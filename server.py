2fd585853b07621982b5634b83d458c841ecdcfa
Walter Cheng <walterpakwai@live.com>
2018-01-27 22:45:10 -0500

    nothing

--- server.py
+++ server.py
@@ -1,19 +1,26 @@
 #importing library for the server
-from flask import Flask 
-import os
+from flask import Flask, flash, redirect, render_template, request, session, abort
 
+#importing matplotlib
 from matplotlib.backends.backend_agg import FigureCanvasAgg
 from matplotlib.figure import Figure
 
-from flask import Flask, flash, redirect, render_template, request, session, abort
 app = Flask(__name__, template_folder='html')
 
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.figure import Figure
-
+#getting the home page
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+#getting the youtube link
+@app.route('/youtube', methods=['POST'])
+def youtube():
+	if request.method == 'POST':
+		print("ok")
+		return render_template("index.html", result="ok")
+	#path = request.form['link']
+	#if "www.youtube.com" not in path:
+	#	path = "https://www.youtube.com/watch?v="
 
 if __name__ == "__main__":
 	app.run(debug=True)
