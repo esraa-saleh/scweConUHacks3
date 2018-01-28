e235360025606869111f0362605c63374add38d2
Walter Cheng <walterpakwai@live.com>
2018-01-27 22:58:00 -0500

    working

--- server.py
+++ server.py
@@ -1,24 +1,28 @@
 #importing library for the server
 from flask import Flask, flash, redirect, render_template, request, session, abort
-
-#importing matplotlib
-from matplotlib.backends.backend_agg import FigureCanvasAgg
-from matplotlib.figure import Figure
+#importing youtube mp3 converter
+from mp3_youtube import *
 
 app = Flask(__name__, template_folder='html')
 
 #getting the home page
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
 	return render_template('index.html')
 
 #getting the youtube link
-@app.route('/youtube', methods=['POST'])
+@app.route('/', methods=['POST'])
 def youtube():
 	if request.method == 'POST':
-		print("ok")
-		return render_template("index.html", result="ok")
-	#path = request.form['link']
+		path = request.form['url']
+		if "www.youtube.com" not in path:
+			path = "https://www.youtube.com/watch?v=" + path
+		print(path)
+
+		download_mp3(path)
+		
+		return render_template("index.html", result=path)
+	#path = request.form['url']
 	#if "www.youtube.com" not in path:
 	#	path = "https://www.youtube.com/watch?v="
 
