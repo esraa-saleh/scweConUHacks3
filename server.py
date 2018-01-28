3bee63b77919e68e28f0beb048827e0dcc09d715
Walter Cheng <walterpakwai@live.com>
2018-01-28 03:29:34 -0500

    adding info inside html

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
 #importing library for the server
-from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for, Markup
 import os
 #importing youtube mp3 converter
 from mp3_youtube import *
 #importing analyze function
 from ibmWatson import *
+#importing random
+import random
+#importing dictioanry
+from Dictionary import *
+
 
 app = Flask(__name__, template_folder='html', static_url_path='/static')
 
@@ -12,10 +17,12 @@ showing_image = "/static/histogram.png"
 success_image = "/static/output.png"
 failing_image = "/static/notfound.png"
 
+styles = ["success", "danger", "info", "warning", "active"]
+
 #getting the home page
 @app.route('/', methods=['GET'])
 def home():
-	return render_template('index.html', image=showing_image)
+	return render_template('index.html', image=showing_image, words="")
 
 #getting the youtube link
 @app.route('/', methods=['POST'])
@@ -26,6 +33,10 @@ def youtube():
 			path = "https://www.youtube.com/watch?v=" + path
 
 		sending_image = success_image
+
+		#temp data
+		objArray = ['', '', '']
+		tag_value = ""
 		#if successfully download the mp3
 		if download_mp3(path):
 			os.remove("./static/output.png")
@@ -33,6 +44,17 @@ def youtube():
 			if analysis_success:
 				print(objDict)
 				getFreqPlot(objDict, "./static/output.png")
+				objArray = list()
+				for i in objDict.keys():
+					objArray.append(i)
+
+				html_tag = ""
+				for i in range(len(objArray)):
+					#meaning = get_meaning(objArray[i])
+					html_tag += '<tr class="' + random.choice(styles) + '"><td>' + objArray[i] + '</td><td>meaning</td></tr>'
+				
+				tag_value = Markup(html_tag)
+
 			else:
 				sending_image = failing_image
 
@@ -40,11 +62,10 @@ def youtube():
 		else:
 			sending_image = failing_image
 		
-		return render_template("index.html", result=path, image=sending_image)
-	#path = request.form['url']
-	#if "www.youtube.com" not in path:
-	#	path = "https://www.youtube.com/watch?v="
+		return render_template("index.html", result=path, image=sending_image, words=tag_value)
 
+
+#for not caching the images
 @app.after_request
 def add_header(response):
     """
