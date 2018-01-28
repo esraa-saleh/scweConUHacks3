7ff70bc743294eaad1df41ff1255a27d24553f3e
SKapal <sahilkapal@cmail.carleton.ca>
2018-01-27 20:28:57 -0500

    youtube -> mp3

---
+++ mp3_youtube.py
@@ -0,0 +1,14 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+    }],
+}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['http://www.youtube.com/watch?v=BaW_jenozKc'])
